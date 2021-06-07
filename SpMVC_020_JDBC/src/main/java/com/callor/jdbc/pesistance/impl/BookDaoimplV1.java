68e8ff43f29fda1bb35892b1fa9286571317decb
choiseonyoung <bviibb@naver.com>
2021-06-07 11:54:03 +0900

    JDBC

---
+++ SpMVC_020_JDBC/src/main/java/com/callor/jdbc/pesistance/impl/BookDaoimplV1.java
@@ -0,0 +1,76 @@
+package com.callor.jdbc.pesistance.impl;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.callor.jdbc.model.BookVO;
+import com.callor.jdbc.pesistance.BookDao;
+
+public class BookDaoimplV1 implements BookDao {
+
+	// jdbc-context.xml에 선언된 jdbcTemplate bean 사용하기
+	protected final JdbcTemplate jdbcTemplate;
+	public BookDaoimplV1(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+		// xml에 선언된 jdbc 가져와 사용할 준비됨
+	}
+	
+	@Override
+	public List<BookVO> selectAll() {
+		// TODO Auto-generated method stub
+		String sql = "SELECT * FROM tbl_books ";
+		List<BookVO> books = jdbcTemplate.query(sql, new BeanPropertyRowMapper<BookVO>(BookVO.class));
+		return null;
+	}
+
+	@Override
+	public BookVO findById(String pk) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(BookVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(BookVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(String pk) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<BookVO> findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookVO> findByDate(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookVO> findByComp(String comp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookVO> findByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
