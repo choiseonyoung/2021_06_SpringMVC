c572162603853e9044ab890e2e77dac933c7c165
choiseonyoung <bviibb@naver.com>
2021-06-28 15:12:15 +0900

    NaverSearch

--- SpMVC_040_book/src/main/java/com/callor/book/config/NaverSecret.java
+++ SpMVC_040_book/src/main/java/com/callor/book/config/NaverSecret.java
@@ -1,6 +1,6 @@
 package com.callor.book.config;
 
 public class NaverSecret {
-	public static final String NAVER_CLIENT_ID = "";
+	public static final String NAVER_CLIENT_ID = "hi";
 	public static final String NAVER_CLIENT_SECRET = "";
 }
